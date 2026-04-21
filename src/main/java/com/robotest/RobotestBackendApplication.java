a2f928d512e991df3596574d4c226a2452d8246e
parvej <parvej@quantummethod.org.bd>
2026-04-21 17:03:12 +0600

    Added time zone

--- src/main/java/com/robotest/RobotestBackendApplication.java
+++ src/main/java/com/robotest/RobotestBackendApplication.java
@@ -1,14 +1,23 @@
 package com.robotest;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 @EnableAsync
 @EnableScheduling
 public class RobotestBackendApplication {
+
+    @PostConstruct
+    public void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Dhaka"));
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(RobotestBackendApplication.class, args);
     }
